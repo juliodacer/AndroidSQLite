a573ebe327e31557c92352b5d4f1001e0ed34f5f
Julio Davila <juliodacer@gmail.com>
2022-09-26 13:42:14 -0500

    activity registrar usuario

---
+++ app/src/main/java/com/devcer/cursoandroidsqlite/RegisterActiviy.java
@@ -0,0 +1,14 @@
+package com.devcer.cursoandroidsqlite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterActiviy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
